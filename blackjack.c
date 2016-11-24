7c330ebd9c2488041ad96953cc633843bf86a418
Johan Kämpe <kampe.johan@gmail.com>
2016-11-24 20:46:27 +0100

    Init

---
+++ blackjack.c
@@ -0,0 +1,65 @@
+/* 
+
+BlackJack by Johan Kämpe! 
+For C programming practice.
+
+*/
+
+/* 
+Struct for decks
+Values 
+01: ACE 
+11: JACK 
+12: QUEEN
+13: KING
+
+Suits
+0: HEARTS
+1: DIAMONDS
+2: CLUBS
+3: CLUBS
+
+*/
+#include <stdio.h>
+#define SUITS 4
+#define CARDS 52
+#define DECKS 6
+
+struct deck{
+	int suit;
+	int value; 
+};
+
+//Sets card values to deck in play
+void setCards(struct deck *deck);
+//Prints out card values in deck
+void printDeck(struct deck *deck);
+
+int main(){
+	struct deck deck[CARDS*DECKS];
+	setCards(deck);
+	printDeck(deck);
+}
+
+//Sets card values to deck in play
+void setCards(struct deck *deck){
+	int cardNo = 0;
+	while(cardNo < CARDS*DECKS){
+		for(int suit = 0;suit<SUITS;suit++){
+			for(int value=1;value<14;value++){
+				deck[cardNo].suit = suit;
+				deck[cardNo].value = value;
+				cardNo++;
+			}
+		}
+	}
+	return;
+}
+
+//Prints out card values in deck
+void printDeck(struct deck *deck){
+	for(int i=0; i<CARDS*DECKS, i++){
+		
+	}
+	return;
+}
